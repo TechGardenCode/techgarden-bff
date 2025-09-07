4540cd4bd32897c0c06612f78c7e9389e91025f6
Tech Garden <techgardencode@gmail.com>
2025-09-07 17:08:20 -0400

    feat: add config values for redis

--- src/main/java/gg/techgarden/bff/config/SessionConfig.java
+++ src/main/java/gg/techgarden/bff/config/SessionConfig.java
@@ -1,5 +1,7 @@
 package gg.techgarden.bff.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -7,11 +9,18 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 
 @Configuration
 @EnableRedisHttpSession
+@RequiredArgsConstructor
 public class SessionConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
 
     @Bean
     public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
 }
