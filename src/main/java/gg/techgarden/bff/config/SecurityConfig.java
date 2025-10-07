7aa0667c50f5162436cb76daca67895d765a626c
Tech Garden <techgardencode@gmail.com>
2025-10-07 10:57:58 -0400

    feat: add security for public reacitons

--- src/main/java/gg/techgarden/bff/config/SecurityConfig.java
+++ src/main/java/gg/techgarden/bff/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/blog/posts/metadata").permitAll()
                         .requestMatchers(HttpMethod.GET, "/blog/posts/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/blog/feed/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/blog/reactions/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/profile/profiles/**").permitAll()
                         .requestMatchers("/blog/**").authenticated()
                         .requestMatchers("/test/**").authenticated()
